8bd7bda5f757cd82a48256b63bf8548adceba01a
23A95A1203 <ravitejakatraju73@gmail.com>
2025-08-30 02:02:48 +0530

    Resolved handleCart code

--- src/redux/reducer/handleCart.js
+++ src/redux/reducer/handleCart.js
@@ -1,33 +1,39 @@
 const cart = []
 
-const handleCart = (state=cart, action) =>{
-    const product = action.payload
-    switch(action.type){
+const handleCart = (state = cart, action) => {
+    const product = action.payload;
+    switch (action.type) {
         case "ADDITEM":
-            // Check if product already in cart
-            const exist = state.find((x) => x.id === product.id)
-            if(exist){
-                // Increase the quantity
-                return state.map((x)=>x.id ===product.id?{...x, qty: x.qty+1}:x)
+            // Check if product is already in cart
+            const exist = state.find((x) => x.id === product.id);
+            if (exist) {
+                // If it exists, increase the quantity
+                return state.map((x) =>
+                    x.id === product.id ? { ...x, qty: x.qty + 1 } : x
+                );
+            } else {
+                // If it does not exist, add the new product
+                return [
+                    ...state, { ...product, qty: 1 }
+                ];
             }
-            else{
-                return [...state, {...product, qty:1}]
-            }
-            break;
+
         case "DELITEM":
-            const exist2 = state.find((x) => x.id === product.id)
-            if(exist2.qty === 1){
-                return state.filter((x)=>x.id!==exist2.id)
+            // Check if the product's quantity is 1
+            const exist2 = state.find((x) => x.id === product.id);
+            if (exist2.qty === 1) {
+                // If it is, remove the product from the cart
+                return state.filter((x) => x.id !== exist2.id);
+            } else {
+                // Otherwise, decrease the quantity
+                return state.map((x) =>
+                    x.id === product.id ? { ...x, qty: x.qty - 1 } : x
+                );
             }
-            else{
-                return state.map((x)=> x.id===product.id?{...x, qty:x.qty-1}:x)
-            }
-            break;
 
         default:
-            return state
-            break;
+            return state;
     }
 }
 
-export default handleCart
+export default handleCart;
